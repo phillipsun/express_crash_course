dca86154fd90877b9ff5322d408dc89b24189cf4
Phil Sun <psun.04@gmail.com>
2020-03-26 23:02:11 -0500

    added express-handlebars package to render views & created simple views

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route (usually it's either this or setting a static folder below)
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Members App',
+    members
+  });
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
