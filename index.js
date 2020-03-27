a194d43c9227861ec64ccb7b15970937e9f00f0c
Phil Sun <psun.04@gmail.com>
2020-03-26 20:53:27 -0500

    created simple get endpoint which returns hardcoded data for now

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const path = require('path');
 
 const app = express();
 
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Phil Sun',
+    email: 'phil@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Shannon Jack',
+    email: 'sjack@gmail.com',
+    status: 'active'
+  }
+];
+
+// Gets All Members
+app.get('/api/members', (req, res) => {
+  res.json(members);
+});
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
