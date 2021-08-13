4f534093f87e28fe162bc2b62907e4ed165b9539
Phil Sun <psun.04@gmail.com>
2021-08-12 22:32:25 -0500

    add new pkgs to test upload route

--- test/members.test.js
+++ test/members.test.js
@@ -6,6 +6,10 @@ const path = require('path')
 const memberList = require('../Members')
 const app = require('../index')
 chai.use(chaiHttp)
+const request = require('request')
+const sinon = require('sinon')
+const should = chai.should();
+const router = require('../routes/api/members')
 
 describe('Unit Tests', function () {
   it('member list is an array', () => {
@@ -22,16 +26,16 @@ describe('Unit Tests', function () {
       })
   })
 
-  it('GET members should return an array', (done) => {
-    chai
-      .request(app)
-      .get('/api/members')
-      .end(function (err, res) {
-        expect(res).to.have.status(200)
-        expect(res.body).to.be.an('array')
-        done()
-      })
-  })
+  // it('GET members should return an array', (done) => {
+  //   chai
+  //     .request(app)
+  //     .get('/api/members')
+  //     .end(function (err, res) {
+  //       expect(res).to.have.status(200)
+  //       expect(res.body).to.be.an('array')
+  //       done()
+  //     })
+  // })
 
   it('GET single member should return an array', (done) => {
     chai
@@ -156,4 +160,99 @@ describe('Unit Tests', function () {
         done()
       })
   })
+
+  var requester = chai.request(app).keepOpen()
+  beforeEach(() => {
+    this.get = sinon.stub(requester, 'get').returns({ hi: 'hello '});
+  });
+
+  afterEach(() => {
+    requester.get.restore();
+  });
+
+  describe('GET all members', () => {
+    const responseObject = {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json'
+      }
+    };
+    const responseBody = {
+      status: 'success',
+      data: [
+        {
+          id: 4,
+          name: 'The Land Before Time',
+          genre: 'Fantasy',
+          rating: 7,
+          explicit: false
+        },
+        {
+          id: 5,
+          name: 'Jurassic Park',
+          genre: 'Science Fiction',
+          rating: 9,
+          explicit: true
+        },
+        {
+          id: 6,
+          name: 'Ice Age: Dawn of the Dinosaurs',
+          genre: 'Action/Romance',
+          rating: 5,
+          explicit: false
+        }
+      ]
+    };
+    it('should return all movies', (done) => {
+
+
+      chai.request(app).get('/api/members').end((err, res) => {
+        expect(res).not.to.be.null
+        done()
+      })
+
+
+
+
+      ///////
+
+      // Promise.all([
+      //   requester.get('/api/members'),
+      // ])
+      // .then(responses => console.log(responses[0]))
+      // .then(() => {
+      //   requester.close()
+      //   done()
+      // })
+
+
+
+      /////
+
+
+      // this.get.yields(null, responseObject, JSON.stringify(responseBody));
+      // request.get('http://localhost:5000/api/members', (err, res, body) => {
+      //   // there should be a 200 status code
+      //   res.statusCode.should.eql(200);
+      //   // the response should be JSON
+      //   res.headers['content-type'].should.contain('application/json');
+      //   // parse response body
+      //   body = JSON.parse(body);
+      //   // the JSON response body should have a
+      //   // key-value pair of {"status": "success"}
+      //   body.status.should.eql('success');
+      //   // the JSON response body should have a
+      //   // key-value pair of {"data": [3 movie objects]}
+      //   body.data.length.should.eql(3);
+      //   // the first object in the data array should
+      //   // have the right keys
+      //   body.data[0].should.include.keys(
+      //     'id', 'name', 'genre', 'rating', 'explicit'
+      //   );
+      //   // the first object should have the right value for name
+      //   body.data[0].name.should.eql('The Land Before Time');
+      //   done();
+      // });
+    });
+  });
 })
